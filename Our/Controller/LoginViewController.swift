15ca4ea2ba10f3f456b77583cf71ca733f8822b1
LeeSe0ngYe0n <goblin__@naver.com>
2024-03-09 17:08:18 +0900

    feat: LoginViewController 구현

---
+++ Our/Controller/LoginViewController.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+final class LoginViewController: UIViewController {
+    
+    let login: LoginView = LoginView()
+    
+    override func loadView() {
+        view = LoginView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setButtonAddTarget()
+    }
+    
+    private func setButtonAddTarget() {
+        guard let loginView = view as? LoginView else { return }
+        loginView.forgotPasswordButton.addTarget(self, action: #selector(tappedForgotPasswordButton), for: .touchUpInside)
+        loginView.loginButton.addTarget(self, action: #selector(tappedLoginButton), for: .touchUpInside)
+    }
+    
+    @objc private func tappedForgotPasswordButton() {
+        //
+    }
+    
+    @objc private func tappedLoginButton() {
+        guard let loginView = view as? LoginView else { return }
+        if loginView.emailTextField.text != "lsyeon98@naver.com" && login.passwordTextField.text != "123" {
+            loginView.checkEmailAndPasswordLabel.textColor = .systemRed
+        }
+    }
+}
